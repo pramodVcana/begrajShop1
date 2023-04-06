e3e7b5944958642b61bede1026017b189b16ae40
Pramod Vcana <pramod@vcanaglobal>
2023-04-06 09:24:39 +0530

    initial commit -m

---
+++ routes/bootcamps.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload,
+} = require("../controller/bootcamp");
+
+const Bootcamp = require("../models/Bootcamp");
+const advancedResults = require("../middlewares/advancedResults");
+
+// Include other resource routers
+const courseRouter = require("./course");
+
+const router = express.Router();
+
+// Re-route into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
+router.route("/:id/photo").put(bootcampPhotoUpload);
+
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
